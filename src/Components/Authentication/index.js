9239933abbd5052d00dfe50ec6f71b58fc17c870
Vrezh <hovanisyan.vrezh@gmail.com>
2018-07-11 00:17:35 +0400

    [add] login/logout rendering to Auth

--- src/Components/Authentication/index.js
+++ src/Components/Authentication/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import LogIn from './LogIn';
-import LogOut from './LogOut';
 
 class Authentication extends React.Component {
   render() {
     const { isLoggedIn, logInToggle } = this.props;
 
     return isLoggedIn ? (
-      <LogOut logInToggle={logInToggle} />
+      <div>
+        <h2>Are you sure you want to log out</h2>
+        <button onClick={logInToggle}>YES!</button>
+      </div>
     ) : (
-      <LogIn logInToggle={logInToggle} />
+      <div>
+        <h2>You should Log In!</h2>
+        <button onClick={logInToggle}>Log In</button>
+      </div>
     );
   }
 }
