dcb724cf9c7525ee70c5c8b31bb66f828d7d5bed
Vrezh <hovanisyan.vrezh@gmail.com>
2018-07-10 21:29:50 +0400

    [add] logging state to Main

--- src/Components/Main/index.js
+++ src/Components/Main/index.js
@@ -5,15 +5,41 @@ import ScrollToTop from '../../Helpers/Scroll/';
 import MainNav from '../MainNav';
 import Authentication from '../Authentication';
 class Main extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoggedIn: false,
+    };
+
+    this.logInToggle = this.logInToggle.bind(this);
+  }
+
+  logInToggle(value) {
+    this.setState({
+      isLoggedIn: value,
+    });
+  }
+
   render() {
+    const { isLoggedIn } = this.state;
+
     return (
       <Router>
         <ScrollToTop>
-          <MainNav />
+          <MainNav isLoggedIn={isLoggedIn} />
 
           <Switch>
             <Route exact path="/" render={() => <h1>Blogs</h1>} />
-            <Route path="/login" component={Authentication} />
+            <Route
+              path="/login"
+              render={() => (
+                <Authentication
+                  isLoggedIn={isLoggedIn}
+                  logInToggle={this.logInToggle}
+                />
+              )}
+            />
             <Route path="/blog/create" render={() => <h1>Create Post</h1>} />
           </Switch>
         </ScrollToTop>
