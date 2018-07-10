757b6bd133c0509be78f835bbf2d346e002fbb23
Vrezh <hovanisyan.vrezh@gmail.com>
2018-07-11 01:26:22 +0400

    [add] login Form support

--- src/Components/Authentication/index.js
+++ src/Components/Authentication/index.js
@@ -1,18 +1,32 @@
 import React from 'react';
+import LogInForm from './LogInForm';
+import LogOutForm from './LogOutForm';
 
 class Authentication extends React.Component {
   render() {
-    const { isLoggedIn, logInToggle } = this.props;
+    const {
+      isLoggedIn,
+      logInFormClose,
+      addUserAndLogIn,
+      logOut,
+      isLogInFormOpen,
+    } = this.props;
 
     return isLoggedIn ? (
       <div>
-        <h2>Are you sure you want to log out</h2>
-        <button onClick={logInToggle}>YES!</button>
+        <LogOutForm
+          isLogInFormOpen={isLogInFormOpen}
+          logInFormClose={logInFormClose}
+          logOut={logOut}
+        />
       </div>
     ) : (
       <div>
-        <h2>You should Log In!</h2>
-        <button onClick={logInToggle}>Log In</button>
+        <LogInForm
+          isLogInFormOpen={isLogInFormOpen}
+          logInFormClose={logInFormClose}
+          addUserAndLogIn={addUserAndLogIn}
+        />
       </div>
     );
   }
