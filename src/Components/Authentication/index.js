9b682320b63e7da65e4854d1849d9aa1ae17cd66
Vrezh <hovanisyan.vrezh@gmail.com>
2018-07-10 21:29:19 +0400

    [del] state from Auth

--- src/Components/Authentication/index.js
+++ src/Components/Authentication/index.js
@@ -3,29 +3,13 @@ import LogIn from './LogIn';
 import LogOut from './LogOut';
 
 class Authentication extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isLoggedIn: false,
-    };
-
-    this.logInToggle = this.logInToggle.bind(this);
-  }
-
-  logInToggle(value) {
-    this.setState({
-      isLoggedIn: value,
-    });
-  }
-
   render() {
-    const { isLoggedIn } = this.state;
+    const { isLoggedIn, logInToggle } = this.props;
 
     return isLoggedIn ? (
-      <LogIn logInToggle={this.logInToggle} />
+      <LogIn logInToggle={logInToggle} />
     ) : (
-      <LogOut logInToggle={this.logInToggle} />
+      <LogOut logInToggle={logInToggle} />
     );
   }
 }
