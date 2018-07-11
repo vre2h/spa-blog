72dc4d416341dda6e5a87f2c33a2206e0875e6e0
Vrezh <hovanisyan.vrezh@gmail.com>
2018-07-11 16:31:49 +0400

    [add] userId and addPost to CreatePost

--- src/Components/ProtectedRoute/index.js
+++ src/Components/ProtectedRoute/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
-const ProtectedRoute = ({ component: Component, isLoggedIn, ...rest }) => (
+const ProtectedRoute = ({
+  component: Component,
+  isLoggedIn,
+  addPost,
+  userId,
+  ...rest
+}) => (
   <Route
     {...rest}
     render={props =>
       isLoggedIn ? (
-        <Component {...props} />
+        <Component {...props} addPost={addPost} userId={userId} />
       ) : (
         <Redirect
           to={{
