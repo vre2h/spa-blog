bc480383b3a5784ea7a03b5ef391281c49a08082
Vrezh <hovanisyan.vrezh@gmail.com>
2018-07-11 19:06:50 +0400

    [del] unnecessary  imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import Main from './Components/Main';
 
