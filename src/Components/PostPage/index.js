4c8f78b795f8a51be484ac0a4bfd61b776826529
Vrezh <hovanisyan.vrezh@gmail.com>
2018-07-12 17:16:09 +0400

    [fix] error if post is undefined

--- src/Components/PostPage/index.js
+++ src/Components/PostPage/index.js
@@ -2,12 +2,25 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 
 import SinglePost from '../SinglePost';
-
+import { Redirect } from 'react-router-dom';
 class PostPage extends React.Component {
   render() {
-    const { postId, posts, users } = this.props;
+    const {
+      postId,
+      posts,
+      users,
+      handleEditPost,
+      handleDeletePost,
+    } = this.props;
+
     const [neededPost] = posts.filter(post => +post.id === +postId);
+
+    if (!neededPost) {
+      return <Redirect to="/" />;
+    }
+
     const { userId, title, content, date, id } = neededPost;
+
     return (
       <Grid container justify="center">
         <Grid key={id} item xs={12} sm={9}>
@@ -19,6 +32,8 @@ class PostPage extends React.Component {
             id={id}
             users={users}
             isPostPage={true}
+            handleEditPost={handleEditPost}
+            handleDeletePost={handleDeletePost}
           />
         </Grid>
       </Grid>
