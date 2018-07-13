8091afafcb7d4ff2257bb3a102b827d5b9c053ac
Vrezh <hovanisyan.vrezh@gmail.com>
2018-07-13 15:19:30 +0400

    [add] single comment

---
+++ src/Components/Comments/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+
+import SingleComment from '../SingleComment';
+
+class Posts extends React.Component {
+  render() {
+    const {
+      comments,
+      users,
+      handleEditComment,
+      handleDeleteComment,
+    } = this.props;
+
+    return (
+      <Grid container justify="center">
+        {comments.map(({ userId, title, content, date, id, postId }) => (
+          <Grid key={id} item xs={12} sm={9}>
+            <SingleComment
+              userId={userId}
+              title={title}
+              content={content}
+              date={date}
+              id={id}
+              users={users}
+              postId={postId}
+              handleEditComment={handleEditComment.bind(this, {
+                title,
+                content,
+                id,
+                date,
+                userId,
+                postId,
+              })}
+              handleDeleteComment={handleDeleteComment.bind(this, {
+                title,
+                content,
+                id,
+                date,
+                userId,
+                postId,
+              })}
+            />
+          </Grid>
+        ))}
+      </Grid>
+    );
+  }
+}
+
+export default Posts;
