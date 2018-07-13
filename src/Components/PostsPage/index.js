f23aa218108d543880712901edf1bcdc132fbd5f
Vrezh <hovanisyan.vrezh@gmail.com>
2018-07-13 17:12:19 +0400

    [add] if there is no post

--- src/Components/PostsPage/index.js
+++ src/Components/PostsPage/index.js
@@ -7,10 +7,20 @@ class Posts extends React.Component {
   render() {
     const { posts, users } = this.props;
 
+    if (posts.length === 0) {
+      return (
+        <Grid container direction="column" alignItems="center" justify="center">
+          <h2>There is no post on web-site.</h2>
+          <h1>Log in and be our first story teller.</h1>
+        </Grid>
+      );
+    }
+
     return (
-      <Grid container justify="center">
+      <Grid container direction="column" alignItems="center">
+        <h1>Posts:</h1>
         {posts.map(({ userId, title, content, date, id }) => (
-          <Grid key={id} item xs={12} sm={9}>
+          <Grid style={{ width: '100%' }} key={id} item xs={12} sm={9}>
             <SinglePost
               userId={userId}
               title={title}
