dba07ea286041e075a0910b37e3a7e60272ec21f
Vrezh <hovanisyan.vrezh@gmail.com>
2018-07-15 03:05:49 +0400

    [refactoring] change styles

--- src/Components/CreateComment/index.js
+++ src/Components/CreateComment/index.js
@@ -1,25 +1,16 @@
 import React from 'react';
 
 import { withStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import DoneIcon from '@material-ui/icons/Done';
-import DeleteIcon from '@material-ui/icons/Delete';
-import EditIcon from '@material-ui/icons/Edit';
 
 const styles = {
-  root: {},
-
-  container: {
-    margin: '20px',
+  'comment-box': {
     padding: '20px',
-  },
-
-  button: {
-    margin: '10px',
+    border: '1px solid #eee',
+    borderRadius: '5px',
   },
 };
 
@@ -28,7 +19,6 @@ class CreateComment extends React.Component {
     super(props);
 
     this.state = {
-      title: '',
       id: '',
       content: '',
       userId: '',
@@ -36,17 +26,10 @@ class CreateComment extends React.Component {
     };
 
     this.handleComment = this.handleComment.bind(this);
-    this.handleTitle = this.handleTitle.bind(this);
     this.handleContent = this.handleContent.bind(this);
   }
 
-  static commentId = 10;
-
-  handleTitle(event) {
-    this.setState({
-      title: event.target.value,
-    });
-  }
+  static commentId = 0;
 
   handleContent(event) {
     this.setState({
@@ -56,90 +39,50 @@ class CreateComment extends React.Component {
 
   handleComment() {
     const { addComment, user, post } = this.props;
-    const { title, content } = this.state;
+    const { content } = this.state;
 
     addComment({
       userId: user.id,
       postId: post.id,
-      title,
       content,
       id: (CreateComment.commentId += 1),
       date: ('' + new Date()).substr(4, 11),
     });
-    this.setState({ id: '', title: '', content: '', userId: '', postId: '' });
+
+    this.setState({ id: '', content: '', userId: '', postId: '' });
   }
 
   render() {
     const { classes } = this.props;
-    const { title, content } = this.state;
+    const { content } = this.state;
 
     return (
       <Grid container justify="center">
         <Grid item xs={12} sm={9}>
-          <Paper className={classes.container}>
-            <Typography align="center" color="secondary" variant="title">
-              Tell us your story!
-            </Typography>
-            <Grid container>
-              <Grid item xs={12} sm={9}>
-                <TextField
-                  error={title.trim() === '' ? true : false}
-                  fullWidth
-                  required
-                  label="Title"
-                  value={title}
-                  className={classes.textField}
-                  margin="normal"
-                  placeholder="..."
-                  onChange={this.handleTitle}
-                />
-              </Grid>
-            </Grid>
-            <TextField
-              error={content.trim() === '' ? true : false}
-              label="Content"
-              margin="normal"
-              value={content}
-              required
-              fullWidth
-              multiline
-              rows={4}
-              rowsMax={5}
-              placeholder="Write your comment..."
-              onChange={this.handleContent}
-            />
-            <Grid container direction="row-reverse">
-              <Button
-                variant="fab"
-                color="primary"
-                aria-label="add"
-                className={classes.button}
-                onClick={this.handleComment}
-                disabled={
-                  title.trim() === '' || content.trim() === '' ? true : false
-                }
-              >
-                <DoneIcon />
-              </Button>
-              <Button
-                variant="fab"
-                disabled
-                color="secondary"
-                aria-label="edit"
-                className={classes.button}
-              >
-                <EditIcon />
-              </Button>
-              <Button
-                variant="fab"
-                disabled
-                aria-label="delete"
-                className={classes.button}
-              >
-                <DeleteIcon />
-              </Button>
-            </Grid>
-          </Paper>
+          <TextField
+            label="Comment"
+            value={content}
+            required
+            fullWidth
+            multiline
+            rows={4}
+            rowsMax={5}
+            placeholder="Write your comment..."
+            onChange={this.handleContent}
+            margin="normal"
+          />
+          <Grid container direction="row-reverse">
+            <Button
+              variant="fab"
+              color="primary"
+              aria-label="add"
+              className={classes.button}
+              onClick={this.handleComment}
+              disabled={content.trim() === '' ? true : false}
+            >
+              <DoneIcon />
+            </Button>
+          </Grid>
         </Grid>
       </Grid>
     );
