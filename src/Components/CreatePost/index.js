17f074574087fd63e52920f76c4a8d2569ef25ac
Vrezh <hovanisyan.vrezh@gmail.com>
2018-07-15 03:13:49 +0400

    [refactoring] styles

--- src/Components/CreatePost/index.js
+++ src/Components/CreatePost/index.js
@@ -8,19 +8,11 @@ import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import DoneIcon from '@material-ui/icons/Done';
-import DeleteIcon from '@material-ui/icons/Delete';
-import EditIcon from '@material-ui/icons/Edit';
 
 const styles = {
-  root: {},
-
   container: {
     margin: '20px',
     padding: '20px',
-  },
-
-  button: {
-    margin: '10px',
   },
 };
 
@@ -29,6 +21,7 @@ class CreatePost extends React.Component {
     super(props);
 
     this.state = {
+      redirectToReferrer: false,
       title: '',
       id: '',
       content: '',
@@ -38,13 +31,6 @@ class CreatePost extends React.Component {
     this.handlePost = this.handlePost.bind(this);
     this.handleTitle = this.handleTitle.bind(this);
     this.handleContent = this.handleContent.bind(this);
-    this.handleRedirect = this.handleRedirect.bind(this);
-  }
-
-  handleRedirect() {
-    this.setState({
-      redirectToReferrer: true,
-    });
   }
 
   static postId = 0;
@@ -72,38 +58,37 @@ class CreatePost extends React.Component {
       id: (CreatePost.postId += 1),
       date: ('' + new Date()).substr(4, 11),
     });
-    this.setState({ id: '', title: '', content: '', userId: '' });
-    this.handleRedirect();
+    this.setState({
+      redirectToReferrer: true,
+      id: '',
+      title: '',
+      content: '',
+      userId: '',
+    });
   }
 
   render() {
     const { classes } = this.props;
-    const { title, content } = this.state;
-    const { redirectToReferrer } = this.state;
-    const { referrer } = this.props.location.state || {
-      referrer: { pathname: '/' },
-    };
+    const { title, content, redirectToReferrer } = this.state;
 
     if (redirectToReferrer) {
-      return <Redirect to={referrer} />;
+      return <Redirect to="/spa-blog" />;
     }
 
     return (
       <Grid container justify="center">
         <Grid item xs={12} sm={9}>
           <Paper className={classes.container}>
-            <Typography align="center" color="secondary" variant="title">
+            <Typography align="center" variant="title">
               Tell us your story!
             </Typography>
             <Grid container>
               <Grid item xs={12} sm={9}>
                 <TextField
-                  error={title.trim() === '' ? true : false}
                   fullWidth
                   required
                   label="Title"
                   value={title}
-                  className={classes.textField}
                   margin="normal"
                   placeholder="..."
                   onChange={this.handleTitle}
@@ -111,7 +96,6 @@ class CreatePost extends React.Component {
               </Grid>
             </Grid>
             <TextField
-              error={content.trim() === '' ? true : false}
               label="Content"
               margin="normal"
               value={content}
@@ -130,28 +114,8 @@ class CreatePost extends React.Component {
                 aria-label="add"
                 className={classes.button}
                 onClick={this.handlePost}
-                disabled={
-                  title.trim() === '' || content.trim() === '' ? true : false
-                }
               >
                 <DoneIcon />
-              </Button>
-              <Button
-                variant="fab"
-                disabled
-                color="secondary"
-                aria-label="edit"
-                className={classes.button}
-              >
-                <EditIcon />
-              </Button>
-              <Button
-                variant="fab"
-                disabled
-                aria-label="delete"
-                className={classes.button}
-              >
-                <DeleteIcon />
               </Button>
             </Grid>
           </Paper>
